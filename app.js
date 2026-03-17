6460fa8ccacca69fe4a523feefa5a0cb68da5f32
Vimukthi-Randunu <wkvp.randunu@gmail.com>
2026-03-17 21:21:30 +0530

    Install the Express and make App.js run on webserver

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
+const express = require('express');
+const app = express();
+
 function subtract(a, b) {
     return a - b;
 }
 
-module.exports = subtract;
+app.get('/', (req, res) => {
+    res.send('App is running');
+});
+
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = { subtract, app };
