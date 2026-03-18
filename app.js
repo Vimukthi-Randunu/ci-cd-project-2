14fe596bf295c16a13475d34737803f3f703d106
Vimukthi-Randunu <wkvp.randunu@gmail.com>
2026-03-18 13:52:50 +0530

    add server.js and update app.js

--- app.js
+++ app.js
@@ -13,9 +13,4 @@ app.get('/health', (req, res) => {
     res.status(200).send('OK');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
 module.exports = { subtract, app };
