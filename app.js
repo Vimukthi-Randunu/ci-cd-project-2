23d995e5a5a8ac38d781c236415ef68db159b4b2
Vimukthi-Randunu <wkvp.randunu@gmail.com>
2026-03-18 16:23:12 +0530

    test the rollback by adding error

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/health', (req, res) => {
-    res.status(200).send('OK');
+    res.status(500).send('error');
 });
 
 module.exports = { subtract, app };
